cc4548a5acd11672d3f437f7203bf5178b559dab
ayushvarshn <43218356+ayushvarshn@users.noreply.github.com>
2024-03-25 23:31:20 +0530

    Update bfs.java
    Added method

--- bfs.java
+++ bfs.java
@@ -1,19 +1,22 @@
 // BFS Algorithm
-Queue<Integer> queue = new LinkedList<>();
-boolean[] visited = new boolean[n];
-
-visited[startNode] = true;
-queue.add(startNode);
-
-while (!queue.isEmpty()) {
-    int q = queue.size()
-    for(int i=0;i<q;i++){
-      int curr = queue.poll();
-      for (int neigh : adjList[curr]) {
-          if (!visited[neigh]) {
-              visited[neigh] = true;
-              queue.add(neigh);
-          }
-      }
-  }
+public void BFS(int startNode, List<List<Integer>> adjList){
+    
+    Queue<Integer> queue = new LinkedList<>();
+    boolean[] visited = new boolean[n];
+    
+    visited[startNode] = true;
+    queue.add(startNode);
+    
+    while (!queue.isEmpty()) {
+        int q = queue.size()
+        for(int i=0;i<q;i++){
+            int curr = queue.poll();
+            for (int neigh : adjList.get(curr)) {
+                if (!visited[neigh]) {
+                    visited[neigh] = true;
+                    queue.add(neigh);
+                }
+            }
+        }
+    }
 }
