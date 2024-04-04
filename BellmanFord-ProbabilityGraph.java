acf7d4abb97fa68235943505f49247f0192300ef
ayushvarshn <43218356+ayushvarshn@users.noreply.github.com>
2024-04-04 23:59:38 +0530

    Update BellmanFord-ProbabilityGraph.java

--- BellmanFord-ProbabilityGraph.java
+++ BellmanFord-ProbabilityGraph.java
@@ -1,5 +1,9 @@
 // To find the path with maximum probability using Bellman Ford
-// Bellman ford is generally used in the problems where you don't know going further into an edge will be better or worse. For e.g. negative weights where going an extra mile may deacrease total weight
+// Bellman ford is generally used in the problems where you don't know going further into an edge will be better or worse. (Weight can increase or decrease)
+// For e.g. a-b-c or a-b. In case You don't know which edge is better, you have to use Bellman Ford.
+// For e.g. negative weights where going an extra mile may deacrease total weight
+// Interesting to note that max probability graph can also be solved by djikstra. Why?
+// Because  a-b-c will always be less than a-b. So you are sure that the prob will deacrease. Thus Djikstra can be used.
 
 double[] dist = new double[n];
 
